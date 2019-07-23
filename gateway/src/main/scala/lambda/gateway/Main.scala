796c5f2356f95ef2002ae28942a23ad128fd414d
Guillaume <guillaume.bogard@colisweb.com>
2019-07-23 13:45:48 +0200

    Created main file

---
+++ gateway/src/main/scala/lambda/gateway/Main.scala
@@ -0,0 +1,31 @@
+package lambda.gateway
+
+import cats.effect._
+import lambda.coderunner.infrastructure.SSPTemplateEngine
+import lambda.coderunner.domain.TemplateEngine
+import lambda.courses.domain.CourseRepository
+import lambda.courses.application.InteractiveWidgetHandler.WidgetHandlerContext
+import lambda.coderunner.domain.CodeRunner
+import lambda.coderunner.domain.Language.Scala2
+import lambda.coderunner.infrastructure.ScalaCodeRunner
+
+object Main extends IOApp {
+
+  val templateEngine: TemplateEngine[IO] = new SSPTemplateEngine[IO]
+
+  // TODO : fix this
+  val courseRepository: CourseRepository[IO] = ???
+  val scala2CodeRunner: CodeRunner[IO, Scala2.type] = ScalaCodeRunner("", Nil)
+
+  val widgetHandlerContext = WidgetHandlerContext(
+    scala2CodeRunner = scala2CodeRunner,
+    templateEngine = templateEngine
+  )
+
+  implicit val apiContext = Api.Context(
+    courseRepository,
+    widgetHandlerContext
+  )
+
+  def run(args: List[String]): IO[ExitCode] = Api()
+}
