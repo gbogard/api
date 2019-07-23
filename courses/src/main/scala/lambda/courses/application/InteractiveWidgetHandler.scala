2ca3b4239e9315480ad89f80a23d76e4abe05f19
Guillaume <guillaume.bogard@colisweb.com>
2019-07-23 15:16:58 +0200

    Updated interactive widget handler

--- courses/src/main/scala/lambda/courses/application/InteractiveWidgetHandler.scala
+++ courses/src/main/scala/lambda/courses/application/InteractiveWidgetHandler.scala
@@ -7,6 +7,7 @@ import cats.data.EitherT
 import lambda.coderunner.domain._
 import lambda.coderunner.domain.Language.Scala2
 import lambda.courses.domain.widgets._
+import lambda.courses.domain.widgets.InteractiveCodeWidget._
 import lambda.courses.domain.widgets.WidgetInput._
 import lambda.courses.domain.widgets.WidgetOutput._
 import lambda.courses.domain.widgets.WidgetError._
@@ -18,7 +19,7 @@ object InteractiveWidgetHandler {
   type Result[F[_]] = EitherT[F, WidgetError, WidgetOutput]
 
   case class WidgetHandlerContext[F[_]: Sync](
-      scala2CodeRunner: ScalaCodeRunner[IO],
+      scala2CodeRunner: ScalaCodeRunner[F],
       templateEngine: TemplateEngine[F]
   )
 
@@ -34,11 +35,9 @@ object InteractiveWidgetHandler {
       implicit ctx: WidgetHandlerContext[F],
       par: Parallel[F, Par]
   ): Result[F] = (widget, input) match {
-    case (w: MultipleChoices, i: AnswerId) => EitherT.fromEither(checkMultipleChoices(w, i))
-    case (w: InteractiveCodeWidget, i: CodeInput) =>
-      if (w.language == i.language) executeInteractiveCode(w, i)
-      else EitherT.leftT(WrongLanguageForWidget)
-    case _ => EitherT.leftT(WrongInputForWidget)
+    case (w: MultipleChoices, i: AnswerId)        => EitherT.fromEither(checkMultipleChoices(w, i))
+    case (w: InteractiveCodeWidget, i: CodeInput) => executeInteractiveCode(w, i)
+    case _                                        => EitherT.leftT(WrongInputForWidget)
   }
 
   private def checkMultipleChoices(
@@ -47,29 +46,34 @@ object InteractiveWidgetHandler {
   ): Either[WidgetError, WidgetOutput] =
     if (input == widget.question.rightAnswer.id) Right(RightAnswer) else Left(WrongAnswer)
 
-  private def selectCodeRunner[F[_]: Sync](
-      language: Language
-  )(implicit ctx: WidgetHandlerContext[F]): EitherT[F, WidgetError, CodeRunnerFn[F]] =
-    language match {
-      case Scala2 => EitherT.rightT(ctx.scala2CodeRunner.run(_))
-      case _      => EitherT.leftT(LanguageIsNotSupported)
-    }
+  private def renderFiles[F[_]: Sync, Par[_]](
+      files: List[SourceFile],
+      userInput: String
+  )(implicit parallel: Parallel[F, Par], ctx: WidgetHandlerContext[F]): Resource[F, List[File]] =
+    for {
+      baseFiles <- Resource.liftF(files.parTraverse(SourceFileHandler.toFile(_)))
+      (templateFiles, basicFiles) = baseFiles.partition(ctx.templateEngine.canRender)
+      params = Map("userInput" -> userInput)
+      output <- ctx.templateEngine.render(templateFiles, params)
+    } yield output
 
   private def executeInteractiveCode[F[_]: Sync, Par[_]](
       widget: InteractiveCodeWidget,
       input: CodeInput
   )(implicit parallel: Parallel[F, Par], ctx: WidgetHandlerContext[F]): Result[F] =
-    selectCodeRunner(widget.language) flatMap { codeRunner =>
-      EitherT {
-        for {
-          baseFiles <- widget.baseFiles.parTraverse(SourceFileHandler.toFile(_))
-          (templateFiles, basicFiles) = baseFiles.partition(ctx.templateEngine.canRender)
-          params = Map("userInput" -> input.code)
-          output <- ctx.templateEngine.render(templateFiles, params) use { renderedTemplateFiles =>
-            val filesToExecute = basicFiles ++ renderedTemplateFiles
-            codeRunner(filesToExecute).map(CodeOutput(_)).leftMap(CodeError(_)).value
-          }
-        } yield output
+    widget match {
+      case s: Scala2CodeWidget if input.language == Scala2 =>
+        EitherT(renderFiles(s.baseFiles, input.code) use { renderedFiles =>
+          processResultToWidgetResult(ctx.scala2CodeRunner.run(renderedFiles, s.mainClass, s.dependencies)).value
+        })
+      case _ => EitherT.leftT(WrongLanguageForWidget)
+    }
+
+  def processResultToWidgetResult[F[_]: Sync](pr: ProcessResult[F]): Result[F] =
+    EitherT {
+      pr.value map {
+        case Left(output)  => Left(CodeError(output))
+        case Right(output) => Right(CodeOutput(output))
       }
     }
 }
--- courses/src/main/scala/lambda/courses/domain/widgets/InteractiveCodeWidget.scala
+++ courses/src/main/scala/lambda/courses/domain/widgets/InteractiveCodeWidget.scala
@@ -2,21 +2,18 @@ package lambda.courses.domain.widgets
 
 import lambda.coderunner.domain._
 import lambda.coderunner.domain.ScalaCodeRunner.ScalaDependency
-import lambda.coderunner.domain.Language.Scala2
 
 sealed trait InteractiveCodeWidget extends InteractiveWidget {
   def defaultValue: String
-  def language: Language
 }
 
 object InteractiveCodeWidget {
   case class Scala2CodeWidget(
-      id: String,
+      id: WidgetId,
       baseFiles: List[SourceFile],
       mainClass: String,
+      defaultValue: String = "",
       dependencies: List[ScalaDependency] = Nil,
       required: Boolean = false
-  ) extends InteractiveCodeWidget {
-      val language = Scala2
-  }
+  ) extends InteractiveCodeWidget
 }
